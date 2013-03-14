5d97453cb4a5d7d82c17fc354b88479a7f07fe81
Ron Jones <rrrron@gmail.com>
2013-03-14 19:50:37 -0400

    deploy 3

--- r2013-03-12-flirt/app/helpers/application_helper.rb
+++ r2013-03-12-flirt/app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def login_nav
     if @auth.present?
-      "<li>#{link_to(@auth.username, '/login', :method => :delete)}</li>"
+      "<li>#{link_to(@auth.username, '/login', :remote => true, :method => :delete)}</li>"
     end
   end
 end
